10b45b94c0d2861c32a2252a96adaecc2d30047d
Aryan Soman <asoman@andrew.cmu.edu>
2022-12-01 20:01:59 -0500

    Add concurrent rebalance test

--- tst/correctness.cpp
+++ tst/correctness.cpp
@@ -15,6 +15,7 @@ struct command1 {
     int tid;
     int totalElements;
     bool shouldFind;
+    bool done;
 };
 
 void *processCommand1(void *command) {
@@ -32,7 +33,24 @@ void *processCommand1(void *command) {
                 comm->t->remove(i);
         }
     }
+    comm->done = true;
     return NULL;
+}
+
+void runRebalanceWhileModifyWorking(LockFullAVLTree *t, command1 *comm, int n) {
+    const int rebalanceGranularity = 10;
+    bool allDone = false;
+    while (!allDone) {
+        // if not all the modify threads are done running, keep rebalancing
+        for (int i = 0; i < rebalanceGranularity; i++) {
+            t->rebalance();
+        }
+        // after "rebalanceGranularity" calls of rebalance, check if the threads are done again
+        allDone = true;
+        for (int i = 0; i < n; i++) {
+            allDone = allDone && comm[i].done;
+        }
+    }
 }
 
 void simpleConcurrentNoRebalance() {
@@ -48,6 +66,7 @@ void simpleConcurrentNoRebalance() {
         commands[i].tid = i;
         commands[i].totalElements = totalElements;
         commands[i].shouldFind = false;
+        commands[i].done = false;
     }
 
     // insert all the elements
@@ -152,8 +171,80 @@ void sequentialEnsureRebalanceReducesUnbalanceToZero() {
     printf("Test sequentialEnsureRebalanceReducesUnbalanceToZero passed!\n");
 }
 
+void concurrentWithRebalance() {
+    printf("Starting test concurrentWithRebalance...\n");
+    LockFullAVLTree *t = new LockFullAVLTree;
+    const int totalElements = 1 << 15;
+
+    // in this case these threads are all modifiers while the current threads runs rebalance
+    pthread_t threads[NUM_THREADS]; 
+    command1 commands[NUM_THREADS];
+    for (int i = 0; i < NUM_THREADS; i++) {
+        commands[i].t = t;
+        commands[i].type = INSERT_TYPE;
+        commands[i].tid = i;
+        commands[i].totalElements = totalElements;
+        commands[i].shouldFind = false;
+        commands[i].done = false;
+    }
+
+    // insert all the elements
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, processCommand1, (void *) &commands[i]);
+    }
+    runRebalanceWhileModifyWorking(t, commands, NUM_THREADS);
+    for (int i = 0; i < NUM_THREADS; i++) {
+       pthread_join(threads[i], NULL);
+    }
+    printf("Inserted all elements\n");
+
+    // search for the elements
+    for (int i = 0; i < NUM_THREADS; i++) {
+        commands[i].type = SEARCH_TYPE;
+        commands[i].shouldFind = true;
+        commands[i].done = false;
+    }
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, processCommand1, (void *) &commands[i]);
+    }
+    runRebalanceWhileModifyWorking(t, commands, NUM_THREADS);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    printf("Found all elements\n");
+
+    // remove all the elements
+    for (int i = 0; i < NUM_THREADS; i++) {
+        commands[i].type = REMOVE_TYPE;
+        commands[i].done = false;
+    }
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, processCommand1, (void *) &commands[i]);
+    }
+    runRebalanceWhileModifyWorking(t, commands, NUM_THREADS);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    printf("Removed all elements\n");
+
+    // search for the elements (they should not be there)
+    for (int i = 0; i < NUM_THREADS; i++) {
+        commands[i].type = SEARCH_TYPE;
+        commands[i].shouldFind = false;
+    }
+    for (int i = 1; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, processCommand1, (void *) &commands[i]);
+    }
+    // runRebalanceWhileModifyWorking(t, commands, NUM_THREADS);
+    for (int i = 1; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    printf("Test concurrentWithRebalance... passed!\n");
+}
+
 int main() {
     simpleSequentialNoRebalance();
     simpleConcurrentNoRebalance(); 
     // sequentialEnsureRebalanceReducesUnbalanceToZero();
+    concurrentWithRebalance();
 }
