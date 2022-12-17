162dc12eb4bb3b3124ed8e903f133964b94366f7
Jerry <jerryx@andrew.cmu.edu>
2022-12-17 04:09:19 -0500

    changed coarse and lockfree output so it's easier to parse

--- testCoarse.bash
+++ testCoarse.bash
@@ -1,20 +1,22 @@
 # CoarseAVLTree Performance Testing
 # ./Performance.exe treeType percentWrite keysetSize numThreads rebalance numOps
 
-n=28
-echo "CoarseAVLTree performance testing with 2^$n operations" >> outputCoarse.txt
-echo "------------------------------------------------------------------------" >> outputCoarse.txt
-echo "" >> outputCoarse.txt
+n=22
+#echo "CoarseAVLTree performance testing with 2^$n operations" >> outputCoarse.txt
+#echo "------------------------------------------------------------------------" >> outputCoarse.txt
+#echo "" >> outputCoarse.txt
 
+echo "keysetSize, percentWrite, numThreads, Ops/mis" >> outputCoarse.txt
 for i in 15 17 19
 do
     for j in 20 40 60
     do
         for k in 1 4 16 32 64
         do
-            echo "Coarse: keysetSize = $i , percentWrite = $j , numThreads = $k" >> outputCoarse.txt
+            #echo "Coarse: keysetSize = $i , percentWrite = $j , numThreads = $k" >> outputCoarse.txt
+            echo -n "$i , $j , $k , " >> outputCoarse.txt
             ./Performance.exe 2 $j $i $k 0 $n >> outputCoarse.txt
-            echo "" >> outputCoarse.txt
+            #echo "" >> outputCoarse.txt
         done
     done
 done
--- testLockFree.bash
+++ testLockFree.bash
@@ -1,20 +1,22 @@
 # LockFree Performance Testing
 # ./Performance.exe treeType percentWrite keysetSize numThreads rebalance numOps
 
-n=5
-echo "LockFree performance testing with 2^$n operations" >> outputLockFree.txt
-echo "------------------------------------------------------------------------" >> outputLockFree.txt
-echo "" >> outputLockFree.txt
+n=22
+#echo "LockFree performance testing with 2^$n operations" >> outputLockFree.txt
+#echo "------------------------------------------------------------------------" >> outputLockFree.txt
+#echo "" >> outputLockFree.txt
 
-for i in 15 #17 19
+echo "keysetSize, percentWrite, numThreads, Ops/mis" >> outputLockFree.txt
+for i in 15 17 19
 do
-    for j in 20 #40 60
+    for j in 20 40 60
     do
-        for k in 1 #4 16 32 64
+        for k in 1 4 16 32 64
         do
-            echo "Lockfree: keysetSize = $i , percentWrite = $j , numThreads = $k" >> outputLockFree.txt
-            ./Performance.exe 1 $j $i $k 1 $n >> outputLockFree.txt
-            echo "" >> outputLockFree.txt
+            #echo "Lockfree: keysetSize = $i , percentWrite = $j , numThreads = $k" >> outputLockFree.txt
+            echo -n "$i , $j , $k , " >> outputLockFree.txt
+            ./Performance.exe 1 $j $i $k 0 $n >> outputLockFree.txt
+            #echo "" >> outputLockFree.txt
         done
     done
 done
