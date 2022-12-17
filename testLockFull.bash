2250cd069dfdfc5658130ab009a51a4b3f5cdfbc
Jerry <jerryx@andrew.cmu.edu>
2022-12-17 00:52:16 -0500

    fixed typos in test script

--- testLockFull.bash
+++ testLockFull.bash
@@ -2,7 +2,7 @@
 # ./Performance.exe treeType percentWrite keysetSize numThreads rebalance numOps
 
 n=28
-echo "LockFull performance testing with 1^$n operations" >> outputLockFull.txt
+echo "LockFull performance testing with 2^$n operations" >> outputLockFull.txt
 echo "------------------------------------------------------------------------" >> outputLockFull.txt
 echo "" >> outputLockFull.txt
 
@@ -13,7 +13,7 @@ do
         for k in 1 4 16 32 64
         do
             echo "Lockfull: keysetSize = $i , percentWrite = $j , numThreads = $k" >> outputLockFull.txt
-            ./Performance.exe 0 40 15 1 1 $n >> outputLockFull.txt
+            ./Performance.exe 0 $j $i $k 1 $n >> outputLockFull.txt
             echo "" >> outputLockFull.txt
         done
     done
